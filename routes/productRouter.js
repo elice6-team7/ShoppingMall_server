39b8db5f12b6a1493ca7f6c0cf112a848fb54298
[AI6/1반] 조해정 <hj6elice@gmail.com>
2023-02-09 16:04:41 +0900

    patch: pagination 수정2

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -10,9 +10,9 @@ productRouter.patch(
   isAdmin,
   productController.setProduct,
 );
-// products 전체 조회는 페이지네이션 (관리자가 볼 거라서)
+//
 productRouter.get("/products", productController.getProducts);
-// 카테고리 별 조회는 무한 스크롤 (일단 그냥 페이지네이션 구현)
+//
 productRouter.get(
   "/products/category/:categoryId",
   productController.getProductsByCategory,
